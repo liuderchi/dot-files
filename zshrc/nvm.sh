cff1def12b8c8de4527eb987b843bb649a43e39c
liuderchi <liuderchi@gmail.com>
2018-05-15 17:34:12 +0800

    introduce nvmrc

--- zshrc/nvm.sh
+++ zshrc/nvm.sh
@@ -19,3 +19,17 @@ export NVM_DIR="$HOME/.nvm"
 [ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh" --no-use  # just load nvm without doing `nvm use`
 NODE_VERSION='v10.0.0'
 export PATH="$NVM_DIR/versions/node/$NODE_VERSION/bin:$PATH"   # insert path to override built-in node path
+
+# https://medium.com/@kinduff/automatic-version-switch-for-nvm-ff9e00ae67f3
+# place this after nvm initialization!
+autoload -U add-zsh-hook
+load-nvmrc() {
+  if [[ -f .nvmrc && -r .nvmrc ]]; then
+    nvm use
+  elif [[ $(nvm version) != $(nvm version default)  ]]; then
+    echo "Reverting to nvm default version"
+    nvm use default
+  fi
+}
+add-zsh-hook chpwd load-nvmrc
+load-nvmrc
