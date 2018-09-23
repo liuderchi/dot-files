767034bf657a287f97d1083deca4f116d5d18c15
liuderchi <liuderchi@gmail.com>
2018-09-23 16:36:22 +0800

    add withWarningMessage HOF

--- .vscode/extensions/derek-custom/src/_util.ts
+++ .vscode/extensions/derek-custom/src/_util.ts
@@ -1,5 +1,7 @@
 'use strict';
 
+import * as vscode from 'vscode';
+
 export const getYearMonthDay: (date?: Date) => [number, string, string] = (
   date = new Date(),
 ) => [
@@ -7,3 +9,11 @@ export const getYearMonthDay: (date?: Date) => [number, string, string] = (
   String(date.getMonth() + 1).padStart(2, '0'),
   String(date.getDate()).padStart(2, '0'),
 ];
+
+export const withWarningMessage = (fcn) => () => {
+  try {
+    fcn();
+  } catch (e) {
+    vscode.window.showWarningMessage(e);
+  }
+}
--- .vscode/extensions/derek-custom/src/markdown.ts
+++ .vscode/extensions/derek-custom/src/markdown.ts
@@ -2,29 +2,31 @@
 
 import * as vscode from 'vscode';
 import * as path from 'path';
-import { getYearMonthDay } from './_util';
+import { getYearMonthDay, withWarningMessage } from './_util';
 
-export const insertInitTemplate = vscode.commands.registerCommand(
+const insertInitTemplate = () => {
+  let editor = vscode.window.activeTextEditor;
+  if (!editor) throw 'No Active Editor';
+
+  const filenameNoExt = /(.*?)(?:\.[^.]+)?$/.exec(
+    path.basename(vscode.window.activeTextEditor.document.fileName),
+  )[1];
+  const fileNameCap = filenameNoExt
+    .replace(/[!-.\:-\@\[-\`\{-~]/g, ' ') // non-Word, /[\W_]/g
+    .split(' ')
+    .map(w => w.charAt(0).toUpperCase() + w.slice(1))
+    .join(' ');
+  const [year, month, day] = getYearMonthDay();
+
+  const template = `# ${fileNameCap}\n\n## Date\n\n- ${year}-${month}-${day}\n\n## Description\n\n-\n__WIP ${year}-${month}-${day}__`;
+
+  editor.edit(edit => edit.insert(new vscode.Position(0, 0), template));
+  // TODO insert at current line
+};
+
+export const insertInitTemplateDisposable = vscode.commands.registerCommand(
   'extension.md.insertInitTemplate',
-  () => {
-    const filenameNoExt = /(.*?)(?:\.[^.]+)?$/.exec(
-      path.basename(vscode.window.activeTextEditor.document.fileName),
-    )[1];
-    const fileNameCap = filenameNoExt
-      .replace(/[!-.\:-\@\[-\`\{-~]/g, ' ') // non-Word, /[\W_]/g
-      .split(' ')
-      .map(w => w.charAt(0).toUpperCase() + w.slice(1))
-      .join(' ');
-    const [year, month, day] = getYearMonthDay();
-
-    const template = `# ${fileNameCap}\n\n## Date\n\n- ${year}-${month}-${day}\n\n## Description\n\n-\n__WIP ${year}-${month}-${day}__`;
-
-    let editor = vscode.window.activeTextEditor;
-    if (!editor) return;
-
-    editor.edit(edit => edit.insert(new vscode.Position(0, 0), template));
-    // TODO insert at current line
-  },
+  withWarningMessage(insertInitTemplate),
 );
 
-export default [insertInitTemplate];
+export default [insertInitTemplateDisposable];
