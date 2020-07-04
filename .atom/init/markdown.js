688329331c244880dc076f9ed44fb9d6d9a161d5
liuderchi <liuderchi@gmail.com>
2020-07-04 11:27:25 +0800

    update(atom): styles and configs

--- .atom/init/markdown.js
+++ .atom/init/markdown.js
@@ -165,6 +165,7 @@ addCmd('atom-text-editor', 'markdown:update-WIP-progress-do-not-jump', requireEd
 }))
 
 // NOTE for all editors with markdown grammar, register command to onWillSave
+// NOTE: temporarily disable (atom v1.33)
 // markdownEditorsOnWillSave('markdown:update-WIP-progress-do-not-jump')
 addCmd('atom-text-editor', 'markdown:get-all-my-buffer-subscriptions', () => console.warn(getMyBufferSubscriptions()))
 addCmd('atom-text-editor', 'markdown:clear-all-my-buffer-subscriptions', () => clearSubcriptions(getMyBufferSubscriptions()))
