bd768699834bdd496b1a13e48f9f531eae6c82c7
liuderchi <liuderchi@gmail.com>
2021-06-28 00:52:51 +0800

    chore: port old extension commands [3]
    - no `use strict` required
    - use `derek-custom` as cmd namespace
    - refactor: split types into each modules

---
+++ derek-custom/src/util.ts
@@ -0,0 +1,21 @@
+import * as vscode from 'vscode';
+import { getFileNameCap, withWarningMessage, withActiveEditor } from './_util';
+
+const insertFunctionalComponent = ({
+  editor,
+}: {
+  editor: vscode.TextEditor;
+}) => {
+  const fileNameCap = getFileNameCap(editor.document.fileName);
+  const template = `import React from 'react';\n\nconst ${fileNameCap} = props => {\n  return (\n    <div>todo</div>;\n  )\n};\n\nexport default ${fileNameCap};`;
+  editor.edit((edit: vscode.TextEditorEdit) =>
+    edit.replace(editor.selection, template),
+  );
+};
+
+export default [
+  vscode.commands.registerCommand(
+    'derek-custom.util.insertFunctionalComponent',
+    withWarningMessage(withActiveEditor(insertFunctionalComponent)),
+  ),
+];
